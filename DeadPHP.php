49572ff084d0caf003f1f8aa745aa253ef6875f9
Kabir Singh <31276824+kabir55@users.noreply.github.com>
2019-06-15 11:49:01 +0530

    Create DeadPHP.php
    PHP code snippet is vulnerable to a command injection attack.
    Shyd yeh nhi chalega.

---
+++ DeadPHP.php
@@ -0,0 +1,23 @@
+<?php
+print("Please specify the name of the file to delete");
+print("<p>");
+$file=$_GET['filename'];
+system("rm $file");
+?>
+
+
+Request
+http://127.0.0.1/delete.php?filename=bob.txt;id
+
+Response
+Please specify the name of the file to delete
+
+uid=33(www-data) gid=33(www-data) groups=33(www-data)
+
+Sanitizing Input
+Replace or Ban arguments with “;”
+Other shell escapes available	
+Example:
+–  &&	
+–  |	
+–  ...	
